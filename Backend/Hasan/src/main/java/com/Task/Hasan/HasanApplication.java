9356c568242cebe1063bd889f11c48f53f415530
Mahthy Hasan <mahthyhasan01@gmail.com>
2025-03-19 01:49:51 +0530

    1-Initiating System with Backend Setup

---
+++ Backend/Hasan/src/main/java/com/Task/Hasan/HasanApplication.java
@@ -0,0 +1,13 @@
+package com.Task.Hasan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HasanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HasanApplication.class, args);
+	}
+
+}
